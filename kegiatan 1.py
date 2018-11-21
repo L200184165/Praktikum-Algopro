bf102125e8b33d6bd9e94652a394dffc49acddaf
L200184165 <45013528+L200184165@users.noreply.github.com>
2018-11-21 13:04:18 +0700

    Delete kegiatan 1.py

--- kegiatan 1.py
+++
@@ -1,8 +0,0 @@
-f = {'segitiga' : 'L = 0.5 * a * t', 'persegi' : 'L = s ** 2', 'persegi panjang' : 'L = p * 1', 'lingkaran' : 'L = pi * r ** 2', 'jajar genjang' : 'L = a * t'}
-print('''no	| nama bangun	| rumus luas
-----|-----------------|-----------------
-1    |segitiga	      |'''+f['segitiga'] +
-'''\n2   |persegi	   |'''+f['persegi'] +
-'''\n3   |persegi panjang  |'''+f['persegi panjang'] +
-'''\n4   |lingkaran	   |'''+f['lingkaran'] +
-'''\n5   |jajar genjang    |'''+f['jajar genjang'])
