137d4ca10a8469c5c2d42098e3ed7914ba95ccdb
L200184165 <45013528+L200184165@users.noreply.github.com>
2018-11-21 13:07:37 +0700

    MODUL PRAKTIKUM 7
    MODUL PRAKTIKUM 7

---
+++ kegiatan 1.py
@@ -0,0 +1,16 @@
+f = {'segitiga' : 'L = 0.5 * a * t', 'persegi' : 'L = s ** 2', 'persegi panjang' : 'L = p * 1', 'lingkaran' : 'L = pi * r ** 2', 'jajar genjang' : 'L = a * t'}
+print('''no	| nama bangun	| rumus luas
+----|-----------------|-----------------
+1    |segitiga	      |'''+f['segitiga'] +
+'''\n2   |persegi	   |'''+f['persegi'] +
+'''\n3   |persegi panjang  |'''+f['persegi panjang'] +
+'''\n4   |lingkaran	   |'''+f['lingkaran'] +
+'''\n5   |jajar genjang    |'''+f['jajar genjang'])
+
+no	| nama bangun	| rumus luas
+----|-----------------|-----------------
+1   |segitiga	      |L = 0.5 * a * t
+2   |persegi	   |L = s ** 2
+3   |persegi panjang  |L = p * 1
+4   |lingkaran	   |L = pi * r ** 2
+5   |jajar genjang    |L = a * t
