ba21a6c806288285c4f31ddd56ef76db22585976
L200184165 <45013528+L200184165@users.noreply.github.com>
2018-11-21 12:58:53 +0700

    MODUL PRAKTIKUM 7
    MODUL PRAKTIKUM 7

---
+++ kegiatan 2.py
@@ -0,0 +1,14 @@
+a = ('Irfan')
+d=0
+while d<3:
+    b = input('masukkan password:')
+    if b == a:
+        print('anda berhasil login')
+        break
+    elif b != a:
+        print('maaf, anda salah memasukkan password.  ')
+        d=d+1
+if d==3:
+    print('maaf anda telah mencoba 3 kali, akses anda di tolak')
+
+
---
+++ kegiatan 3.py
@@ -0,0 +1,16 @@
+a = input('Masukkan Nama Saudara : ')
+b = float(input('Pukul Berapa Sekarang : '))
+if 00.00 <= b <= 03.59:
+    b = "Petang"
+elif 04.00 <= b <= 11.59:
+    b = "Pagi" 
+elif 12.00 <= b <= 14.59 :
+    b = "Siang"
+elif 15.00 <= b <= 17.59 :
+    b = "Sore"
+elif 18.00 <= b <= 23.59 :
+    b = "Malam"
+print("Selamat " + b + " " + a)
+    
+
+
